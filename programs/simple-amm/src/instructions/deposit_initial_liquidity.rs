0dda4286d0556259d073d7f61f8c0f12b73a2540
flubzie <0xflubzie@gmail.com>
2024-01-24 19:31:54 -0800

    add accounts

--- programs/simple-amm/src/instructions/deposit_initial_liquidity.rs
+++ programs/simple-amm/src/instructions/deposit_initial_liquidity.rs
@@ -1,4 +1,8 @@
 use anchor_lang::prelude::*;
+use anchor_spl:: {
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount},
+};
 
 use crate::state::{Amm, Pool};
 
@@ -8,6 +12,7 @@ use crate::state::{Amm, Pool};
 // amm (fee), pool, amm_authority
 // from and to addresses for mint: depositor_account_a, depositor_account_b && pool_account_a, pool_account_b
 // from and to addresses for liquidity: mint_liquidity, depositor_account_liquidity
+// mint_a and mint_b which will be the token pair
 
 // Instruction
 // Take the amounts of each token being passed, create a ratio, calculate liquidity using geo mean,
@@ -37,4 +42,60 @@ pub struct DepositInitialLiquidity<'info> {
         bump,
     )]
     pub pool: Account<'info, Pool>,
+
+    pub mint_a: Box<Account<'info, Mint>>,
+
+    pub mint_b: Box<Account<'info, Mint>>,
+
+    #[account(
+        seeds = [
+            "simple-amm".as_ref(), //@TODO: make this a constant
+            mint_a.key().as_ref(),
+            mint_b.key().as_ref(),
+            "authority".as_ref(), //TODO: make this a constant
+        ],
+        bump
+    )]
+    pub amm_authority: AccountInfo<'info>,
+
+    #[account(
+        associated_token::mint = mint_a,
+        associated_token::authority = depositor,
+    )]
+    pub depositor_account_a: Box<Account<'info, TokenAccount>>,
+
+    #[account(
+        associated_token::mint = mint_a,
+        associated_token::authority = depositor,
+    )]
+    pub depositor_account_b: Box<Account<'info, TokenAccount>>,
+
+    #[account(
+        seeds = [
+            amm.key().as_ref(),
+            mint_a.key().as_ref(),
+            mint_b.key().as_ref(),
+            "authority".as_ref(), //@TODO: make this a constant
+        ],
+        bump
+    )]
+    pub mint_liquidity: Box<Account<'info, Mint>>,
+
+    // associated token account to hold depositor's liquidity tokens
+    #[account(
+        init,
+        payer = payer,
+        associated_token::mint = mint_liquidity,
+        associated_token::authority = depositor,
+    )]
+    pub depositor_account_liqudity: Box<Account<'info, TokenAccount>>,
+
+    pub depositor: Signer<'info>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
 }
